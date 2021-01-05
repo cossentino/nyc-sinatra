9001a681906beb1c101b5319ebc509ccdcea9b2e
Ian Cossentino <icossentino@gmail.com>
2021-01-05 10:51:34 -0500

    commit tree 34d77b2518d92bb5fce0d4dd9dc9d9cb66f126ac

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,55 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    figure = Figure.new(params["figure"])
+    if !params["title"]["name"].empty?
+      figure.titles << Title.new(params["title"])
+    end
+    if !params["landmark"]["name"].empty?
+      figure.landmarks << Landmark.new(params["landmark"])
+    end
+    figure.save
+    redirect to "/figures/#{figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    figure = Figure.find(params[:id])
+    figure.update(params["figure"])
+    if !params["title"]["name"].empty?
+      figure.titles << Title.new(params["title"])
+    end
+    if !params["landmark"]["name"].empty?
+      figure.landmarks << Landmark.new(params["landmark"])
+    end
+    
+    figure.save
+    redirect to "/figures/#{figure.id}"
+  end
+
+
+
 end
